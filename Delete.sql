fffa72f286d01b74848d3f172aab6bf51d48d9af
Siva S <57034234+sivnth07@users.noreply.github.com>
2024-11-18 00:45:24 +0530

    SQL Delete
    https://youtu.be/j7DeEqZ9sLI

---
+++ Delete.sql
@@ -0,0 +1,24 @@
+--Ex 1 :
+
+DELETE FROM Employee WHERE EmployeeID = 1;
+
+
+--Ex 2 :
+
+DELETE FROM Employee WHERE Department = 'HR';
+
+
+--Ex 3 :
+
+DELETE FROM Employee WHERE Salary < 60000;
+
+
+--Ex 4 :
+
+DELETE FROM Employee WHERE Department = 'Sales' AND Salary > 60000;
+
+
+--Ex 5 :
+
+DELETE FROM Employee;
+
