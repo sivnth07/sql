52e798794fbf75d55774aeac6edb7f5aa5df96c3
Siva S <57034234+sivnth07@users.noreply.github.com>
2024-11-29 19:34:11 +0530

    SQL_LIKE
    YouTube Link : https://youtu.be/-NvakbfyhkY
    
    ABOUT THE TOPIC :
    
    The SQL **LIKE** operator is used in a **WHERE** clause to search for a specified pattern in a column. It supports two wildcard characters: **%** (matches any number of characters, including zero) and **_** (matches exactly one character). For example: `SELECT * FROM customers WHERE name LIKE 'A%';` retrieves all customers whose names start with "A".

---
+++ SQL_LIKE/SQL_LIKE.sql
@@ -0,0 +1,29 @@
+--Ex 1 :
+
+SELECT * FROM Employees  
+WHERE Name LIKE 'A%';  
+
+
+--Ex 2 :
+
+SELECT * FROM Employees  
+WHERE Name LIKE '%h';  
+
+
+--Ex 3 :
+
+SELECT * FROM Employees  
+WHERE Email LIKE '%smith%';  
+
+
+--Ex 4 :
+
+SELECT * FROM Employees  
+WHERE Name LIKE 'E%';  
+
+
+--Ex 5 :
+
+SELECT * FROM Employees  
+WHERE Name LIKE 'C%o%';  
+
