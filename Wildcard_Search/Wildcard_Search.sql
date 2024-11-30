3b084d8c5fb829b861a8ec98fc9c9dc10a54a82f
Siva S <57034234+sivnth07@users.noreply.github.com>
2024-11-30 22:53:21 +0530

    Wildcard_Search
    YouTube Link : 
    
    ABOUT THE TOPIC :
    
    Wildcard search in SQL uses special characters to perform flexible pattern matching in queries, often with the **LIKE** operator. Common wildcards include `%` (any number of characters) and `_` (a single character).

---
+++ Wildcard_Search/Wildcard_Search.sql
@@ -0,0 +1,29 @@
+--Ex 1 :
+
+SELECT * FROM Employees  
+WHERE FirstName LIKE 'A%';
+
+
+--Ex 2 :
+
+SELECT * FROM Employees  
+WHERE LastName LIKE '%son%';
+
+
+--Ex 3 :
+
+SELECT * FROM Employees  
+WHERE FirstName LIKE 'D____';
+
+
+--Ex 4 :
+
+SELECT * FROM Employees  
+WHERE LastName LIKE '_r%';
+
+
+--Ex 5 :
+
+SELECT * FROM Employees  
+WHERE FirstName LIKE '[A-C]%';
+
